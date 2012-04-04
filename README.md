02fceeae55fc41b8d123e9df60f2c504bfc4473e
Julien Maupetit <julien@maupetit.net>
2012-04-04 19:26:33 +0200

    Fixed doc typos and adapted module name given its standalone distro

--- README.md
+++ README.md
@@ -48,11 +48,11 @@ Now you need to install satchmo-colissimo from github, either by cloning the pro
 
 ### Activate satchmo-colissimo
 
-To activate your custom shipping module, add `satchmo-colissimo` to your `CUSTOM_SHIPPING_MODULES` list, *e.g.*:
+To activate your custom shipping module, add `satchmo_colissimo` to your `CUSTOM_SHIPPING_MODULES` list, *e.g.*:
 
     SATCHMO_SETTINGS = {
         ....
-	    'CUSTOM_SHIPPING_MODULES':['laposte_colissimo'],
+	    'CUSTOM_SHIPPING_MODULES':['satchmo_colissimo'],
 	}
 
 Now go to your site settings (once logged in the django admin): in development mode it would be something like `http://localhost:8000/settings/`. Click on `Shipping settings` and select `Colissimo` as an activated shipping module. Then, update settings and a new configuration section will appear for [Colissimo shipping module](https://github.com/jmaupetit/satchmo-colissimo).
@@ -64,4 +64,4 @@ Now go to your site settings (once logged in the django admin): in development m
 
 ## Roadmap
 
-* Complete support of SoColissimo WS (multiple delivery modes, tracking number, shipping label, etc.)
+* Complete support of 'So Colissimo' WS (multiple delivery modes, tracking number, shipping label, etc.)
--- satchmo_colissimo/config.py
+++ satchmo_colissimo/config.py
@@ -6,27 +6,27 @@ author: Julien Maupetit (twitter @julienmaupetit)
 from django.utils.translation import ugettext_lazy as _
 from livesettings import *
 
-SHIP_MODULES = config_get('SHIPPING', 'MODULES')
-SHIP_MODULES.add_choice(('laposte_colissimo', 'Colissimo'))
+SHIP_MODULE_NAME = "satchmo_colissimo"
 
-SHIPPING_GROUP = ConfigurationGroup('laposte_colissimo',
-  _('Colissimo Shipping Settings'),
-  requires = SHIP_MODULES,
-  requiresvalue='laposte_colissimo',
-  ordering = 101
-)
+SHIP_MODULES = config_get('SHIPPING', 'MODULES')
+SHIP_MODULES.add_choice((SHIP_MODULE_NAME, 'Colissimo'))
+
+SHIPPING_GROUP = ConfigurationGroup( SHIP_MODULE_NAME,
+                                     _('Colissimo Shipping Settings'),
+                                     requires = SHIP_MODULES,
+                                     requiresvalue = SHIP_MODULE_NAME,
+                                     ordering = 101 )
 
 config_register_list(
+    
     IntegerValue(SHIPPING_GROUP,
                  'RECOMMANDED_DEFAULT_LEVEL',
                  description=_("Colissimo Recommanded default level"),
                  help_text=_("Set the default recommanded level from 0 to 5 corresponding to R0-R5"),
-                 default=0),
+                 default=0 ),
                  
     DecimalValue(SHIPPING_GROUP,
                  'BOX_DEFAULT_WEIGHT',
                  description=_("Colissimo box default weight"),
                  help_text=_("Set the default weight in Kg"),
-                 default=0.2),
-                 
-)
+                 default=0.2) )
